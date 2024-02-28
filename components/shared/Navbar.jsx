e1a19de338cdbc6e33a5217d4bfecf2fe3f2ffd5
Md. Shohidujjaman Sajib <sajibwebdev@gmail.com>
2024-02-28 08:10:18 +0600

    feat: create Navbar

--- components/shared/Navbar.jsx
+++ components/shared/Navbar.jsx
@@ -1,5 +1,22 @@
+import Image from "next/image";
+import logo from "../../assets/images/logo_dark.svg"
 const Navbar = () => {
-  return <div>Navbar</div>;
+  return <nav className="mx-32 my-8">
+  <div className="flex justify-between align-middle">
+    <Image src={logo} alt="logo" width={150} height={93} />
+  <ul className="flex justify-evenly space-x-24">
+    <li>
+      Home
+    </li>
+    <li>
+      About
+    </li>
+    <li>
+      Account
+    </li>
+  </ul>
+  </div>
+  </nav>;
 };
 
 export default Navbar;
